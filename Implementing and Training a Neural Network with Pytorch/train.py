5a5d97daf8a996eccde8f91e071a31b9c20cc030
fine <92whdtjs@gmail.com>
2024-08-03 17:02:15 +0900

    Build Model
    - 기초적인 신경망 구조를 구성합니다.
    - 구성한 신경망 구조를 사용하는 forward 메서드를 구현합니다.

--- Implementing and Training a Neural Network with Pytorch/train.py
+++ Implementing and Training a Neural Network with Pytorch/train.py
@@ -22,6 +22,24 @@ def download_mnist_datasets():
     )
     return train_data, validation_data
 
+# 3 - build model
+class FeedForwardNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.flatten = nn.Flatten()
+        self.dense = nn.Sequential(
+            nn.Linear(28*28, 256),
+            nn.ReLU(),
+            nn.Linear(256, 10)
+        )
+        self.softmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, input_data):
+        flatten_data = self.flatten(input_data)
+        logits = self.dense(flatten_data)
+        predictions = self.softmax(logits)
+        return  predictions
+
 
 if __name__ == "__main__":
     # download MNIST dataset
@@ -31,8 +49,13 @@ if __name__ == "__main__":
     # 2 - create data loader for the train set
     train_data_loader = DataLoader(train_data, batch_size=BATCH_SIZE)
 
-# 3 - build model
-
+    # 3 build model
+    if torch.cuda.is_available():
+        device = "cuda"
+    else:
+        device = "cpu"
+    print(f"Device: {device}")
+    feedforward_net = FeedForwardNet().to(device)
 
 
 # 4 - train
