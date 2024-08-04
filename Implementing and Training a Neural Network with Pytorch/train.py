59afd514e68e4d004214fc152abbc41e9e3e5a24
fine <92whdtjs@gmail.com>
2024-08-04 14:59:02 +0900

    Training Model

--- Implementing and Training a Neural Network with Pytorch/train.py
+++ Implementing and Training a Neural Network with Pytorch/train.py
@@ -5,6 +5,9 @@ from torchvision import datasets
 from torchvision.transforms import ToTensor
 
 BATCH_SIZE = 128
+EPOCHS = 10
+LEARNING_RATE = 0.001
+
 
 # 1 - download dataset
 def download_mnist_datasets():
@@ -40,6 +43,27 @@ class FeedForwardNet(nn.Module):
         predictions = self.softmax(logits)
         return  predictions
 
+def train_one_epoch(model, data_loader, loss_fn, optimizer, device):
+    for input, target in data_loader:
+        input, target = input.to(device), target.to(device)
+
+        # calculate loss
+        predictions = model(input)
+        loss = loss_fn(predictions, target)
+
+        # backpropagation loss update weights
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+    print(f'Loss: {loss.item():.4f}')
+
+def train(model, data_loader, loss_fn, optimizer, device, epochs):
+    for i in range(epochs):
+        print(f'Epoch {i+1}')
+        train_one_epoch(model,data_loader,loss_fn,optimizer,device)
+        print("------------------")
+print("Training is Done")
+
 
 if __name__ == "__main__":
     # download MNIST dataset
@@ -57,7 +81,13 @@ if __name__ == "__main__":
     print(f"Device: {device}")
     feedforward_net = FeedForwardNet().to(device)
 
+    # instantiate loss function and optimizer
+    loss_fn = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(feedforward_net.parameters(), lr=LEARNING_RATE)
 
-# 4 - train
-# 5 - save trained model
+    # 4 train model
+    train(feedforward_net, train_data_loader, loss_fn, optimizer, device, EPOCHS)
 
+    # 5 - save trained model
+    torch.save(feedforward_net.state_dict(), "feedforward_net.pth")
+    print("Model trained and stored at feedforward_net.pth")
